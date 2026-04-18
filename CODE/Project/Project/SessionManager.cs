b622d19fe660d3bbbaf91f48012ce08e6fa6ef86
1circiu1 <patrickcirciu126@gmail.com>
2026-04-19 00:47:58 +0300

    push/

---
+++ CODE/Project/Project/SessionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Models;
+
+namespace Project
+{
+    public static class SessionManager
+    {
+        public static User CurrentUser { get; private set; }
+        public static bool IsLoggedIn => CurrentUser != null;
+        public static bool isAdmin => IsLoggedIn && CurrentUser.Role == "Admin";
+    }
+}
